65db5f6c39eb31e5065dd9b85b1842f7221f7ebd
MrBattka <110553715+MrBattka@users.noreply.github.com>
2023-12-14 10:46:57 +0300

    feat:add-routes

---
+++ boohing/src/pages/Owner/CreateFloorPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateFloorPage = () => {
+  return (
+    <div>CreateFloorPage</div>
+  )
+}
+
+export default CreateFloorPage
---
+++ boohing/src/pages/User/PlanPlacesPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PlanPlacesPage = () => {
+  return (
+    <div>PlanPlaces</div>
+  )
+}
+
+export default PlanPlacesPage
